dc967867667900546705808f2db816e9900c53d7
ElenaVolcov <99703608+ElenaVolcov@users.noreply.github.com>
2022-06-21 22:12:32 -0400

    1st Jenkins

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
 public class Main {
-    public static void main(String[] )
+    public static void main(String[] args) {
     for (int i=0 ;i<5 ;i++){
         System.out.println("hello! am i a cat?");
+        System.out.println("hello! i am a dog?");
     }
+}
 }
